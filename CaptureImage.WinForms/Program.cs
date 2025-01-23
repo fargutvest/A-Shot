0a95f610d2a2b3b6ac44823d4508ec81706dec0b
Farragut West <fargutvest@gmail.com>
2025-01-23 19:20:55 +0300

    added mutex protection from multiple instances

--- CaptureImage.WinForms/Program.cs
+++ CaptureImage.WinForms/Program.cs
@@ -1,19 +1,28 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace CaptureImage.WinForms
 {
     internal static class Program
     {
+        private static string appGuid = "ae14e86b-54fc-4340-9233-017c216803dd";
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AppContext());
+            using (Mutex mutex = new Mutex(false, "Global\\" + appGuid))
+            {
+                if (mutex.WaitOne(0, false) == false)
+                    return;
+           
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new AppContext());
+            }
         }
     }
 }

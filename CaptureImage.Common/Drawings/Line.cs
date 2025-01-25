e92d37caf6deb2598e9ef60d41006e689334a08b
fargutvest <fargutvest@gmail.com>
2025-01-25 22:28:48 +0300

    Pencil lines more streamlined

--- CaptureImage.Common/Drawings/Line.cs
+++ CaptureImage.Common/Drawings/Line.cs
@@ -16,6 +16,9 @@ namespace CaptureImage.Common.Drawings
         public void Paint(Graphics gr, Pen pen)
         {
             gr.DrawLine(pen, start, end);
+
+            gr.FillEllipse(pen.Brush, start.X - pen.Width / 2, start.Y - pen.Width / 2, pen.Width, pen.Width);
+            gr.FillEllipse(pen.Brush, end.X - pen.Width /2, end.Y - pen.Width / 2, pen.Width, pen.Width);
         }
 
         public override string ToString()

ebae1a526f28099b91ccb3cd944dd5f8f5da8c6f
fargutvest <fargutvest@gmail.com>
2025-02-12 08:37:54 +0300

    change cursor over text area

--- CaptureImage.Common/Tools/TextTool/TextTool.cs
+++ CaptureImage.Common/Tools/TextTool/TextTool.cs
@@ -3,6 +3,7 @@ using CaptureImage.Common.Tools.Misc;
 using System.Drawing;
 using System.Windows.Forms;
 using CaptureImage.Common.Drawings;
+using System;
 
 namespace CaptureImage.Common.Tools
 {
@@ -15,7 +16,35 @@ namespace CaptureImage.Common.Tools
         private ICanvas canvas;
         private readonly IDrawingContextProvider drawingContextProvider;
         private DrawingContext.DrawingContext DrawingContext => drawingContextProvider.DrawingContext;
-      
+
+        bool isMoving;
+
+
+        private bool isMouseOver;
+
+        private Point mousePos;
+
+        public bool IsMouseOver
+        {
+            get
+            {
+                return isMouseOver;
+            }
+            private set
+            {
+                if (isMouseOver != value)
+                {
+                    isMouseOver = value;
+
+                    if (isMouseOver)
+                        MouseEnterSelection?.Invoke(this, mousePos);
+                }
+            }
+        }
+
+        public event EventHandler<Point> MouseEnterSelection;
+
+
         public TextTool(IDrawingContextProvider drawingContextProvider, ICanvas canvas)
         {
             this.drawingContextProvider = drawingContextProvider;
@@ -23,13 +52,39 @@ namespace CaptureImage.Common.Tools
             this.state = DrawingState.None;
             textArea = new TextArea(drawingContextProvider);
             textArea.Parent = canvas as Control;
+
+            textArea.MouseDown += TextArea_MouseDown;
+            textArea.MouseUp += TextArea_MouseUp;
         }
-        
+
+        private void TextArea_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMoving = false;
+        }
+
+        private void TextArea_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMoving = true;
+        }
+
         public void MouseMove(Point mouse)
         {
+            mousePos = mousePosition;
+            IsMouseOver = textArea.Bounds.Contains(mousePos);
+
             if (isActive)
             {
+                if (IsMouseOver)
+                {
+                    textArea.Cursor = Cursors.SizeAll;
 
+                    if (isMoving)
+                        textArea.Location = new Point(mouse.X, mouse.Y);
+                }
+                else
+                {
+                    textArea.Cursor = Cursors.Default;
+                }
             }
         }
 
